f8c5bfd7b9ef56d6c007dd26873f3a18857f2310
carson.luan <carson.luan@meetsocial.com>
2022-06-13 14:45:53 +0800

    first commit

---
+++ src/main/java/com/example/learn_backend_java/LearnBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.learn_backend_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnBackendJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnBackendJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learn_backend_java/LearnBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learn_backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnBackendJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
